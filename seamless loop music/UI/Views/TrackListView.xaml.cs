32e8aa114d80a6c7a755d6ad340aa5114c974e9d
cpurising <cpurising@outlook.com>
2026-04-26 13:34:09 +0800

    feat: 实现曲目列表的自动定位功能，支持选中项自动滚动至视口

--- seamless loop music/UI/Views/TrackListView.xaml.cs
+++ seamless loop music/UI/Views/TrackListView.xaml.cs
@@ -8,5 +8,17 @@ namespace seamless_loop_music.UI.Views
         {
             InitializeComponent();
         }
+
+        private void TrackList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (TrackList.SelectedItem != null)
+            {
+                // 延迟一点执行滚动，确保虚拟化容器已经准备好
+                Dispatcher.BeginInvoke(new System.Action(() =>
+                {
+                    TrackList.ScrollIntoView(TrackList.SelectedItem);
+                }), System.Windows.Threading.DispatcherPriority.Background);
+            }
+        }
     }
 }
